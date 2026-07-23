1e67214438c0ad37de98d91bdc8cdaebb648c2a3
aliozank <aliozkaracor@gmail.com>
2026-07-23 09:42:37 +0300

    feat(lab-02): complete Spring REST CRUD basics

---
+++ spring-rest-basics/src/main/java/com/alikaracor/learning/springrestbasics/SpringRestBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.alikaracor.learning.springrestbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestBasicsApplication.class, args);
+    }
+
+}
---
+++ spring-rest-basics/src/test/java/com/alikaracor/learning/springrestbasics/SpringRestBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alikaracor.learning.springrestbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
