a90132e24cc3bb6282b99aa1695b8f3a6ff3a4b3
aliozank <aliozkaracor@gmail.com>
2026-07-24 11:27:50 +0300

    feat(lab-09): implement real-time flight status updates with WebSocket

---
+++ websocket-flight-lab/src/main/java/com/alikaracor/learning/websocketflightlab/WebsocketFlightLabApplication.java
@@ -0,0 +1,13 @@
+package com.alikaracor.learning.websocketflightlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketFlightLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketFlightLabApplication.class, args);
+    }
+
+}
---
+++ websocket-flight-lab/src/test/java/com/alikaracor/learning/websocketflightlab/WebsocketFlightLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alikaracor.learning.websocketflightlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketFlightLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
